58eb87e3077440a4da525b14f4667bb877c123e9
Bibibush <rlawlsgus125@gmail.com>
2024-06-25 22:42:43 +0900

    admin user 페이지 초기 ui 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
-  );
+  return <main>누구나 볼수 있는 페이지 입니다.</main>;
 }
