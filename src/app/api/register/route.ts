3821a64e585a45e582c5f69645b901132310f019
Bibibush <rlawlsgus125@gmail.com>
2024-07-29 00:05:16 +0900

    register 로직 구현 완료

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,8 +1,21 @@
+import client from "@/lib/prismadb";
 import bcrypt from "bcryptjs";
+import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
+  const prisma = client;
   const body = await req.json();
 
   const { email, name, password } = body;
-  const hashedPassword = bcrypt;
+  const hashedPassword = await bcrypt.hash(password, 12);
+
+  const user = await prisma.user.create({
+    data: {
+      email,
+      name,
+      hashedPassword,
+    },
+  });
+
+  return NextResponse.json(user);
 }
