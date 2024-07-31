584712a4f89b97a126d31a7eb2f232781a26d68d
bibibush_jinhyeong <rlawlsgus125@gmail.com>
2024-07-31 11:03:35 +0900

    이메일 중복 체크 로직 작성 및 nextResponse로 에러 구현하기 완료

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,5 +1,6 @@
 import client from "@/lib/prismadb";
 import bcrypt from "bcryptjs";
+import { error } from "console";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
@@ -8,6 +9,15 @@ export async function POST(req: Request) {
 
   const { email, name, password } = body;
   const hashedPassword = await bcrypt.hash(password, 12);
+  const duplicatedEmail = await prisma.user.findUnique({
+    where: {
+      email: email,
+    },
+  });
+
+  if (duplicatedEmail) {
+    return NextResponse.json({ error: "duplicated email" }, { status: 500 });
+  }
 
   const user = await prisma.user.create({
     data: {
