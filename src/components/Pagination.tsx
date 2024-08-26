1d5787e9692ea89a0a1bf5c2adbdf1c01a2d52fc
Bibibush <rlawlsgus125@gmail.com>
2024-08-26 23:05:34 +0900

    페이지네이션 usePagination 사용

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import usePagination from "@lucasmogari/react-pagination";
+import React, { Fragment } from "react";
 
 interface PaginationProps {
   page: number;
@@ -7,7 +8,35 @@ interface PaginationProps {
 }
 
 function Pagination({ page, totalItems, perPage }: PaginationProps) {
-  return <div>Pagination</div>;
+  const { fromItem, getPageItem, toItem, totalPages } = usePagination({
+    totalItems,
+    page,
+    itemsPerPage: perPage,
+    maxPageItems: 3,
+  });
+
+  const firstPage = 1;
+  const nextPage = Math.min(page + 1, totalPages);
+  const prevPage = Math.max(page - 1, firstPage);
+  const arr = new Array(totalPages + 2);
+
+  return (
+    <div>
+      {[...arr].map((_, i) => {
+        const { page, disabled, current } = getPageItem(i);
+        if (page === "previous") {
+          return <span key={i}>{"<"}</span>;
+        }
+        if (page === "next") {
+          return <span key={i}>{">"}</span>;
+        }
+        if (page === "gap") {
+          return <span key={i}>...</span>;
+        }
+        return <span key={i}>{page}</span>;
+      })}
+    </div>
+  );
 }
 
 export default Pagination;
